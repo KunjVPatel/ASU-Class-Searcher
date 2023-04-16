b53e7df5c10158f71f79e7ece4bbcc2029c09fc8
Kunj Patel <98302463+KunjVPatel@users.noreply.github.com>
2023-04-15 17:50:29 -0700

    Add files via upload

--- ASUClassSearcher.py
+++ ASUClassSearcher.py
@@ -1,6 +1,4 @@
-import pandas as pd #Im a data science student what were you expecting I need pandas
-import csv
-import string
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,66 +6,86 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait #type: ignore
 from selenium.webdriver.support import expected_conditions as EC
 
-classNum = 412 #Class Num
-subjectName = "CSE" #Class Subject
+classNum = 301 #Class Num
+subjectName = "DAT" #Class Subject
 termNum = 2237 #Fall 2023 term num
+classNumber = 88817
 
 options = Options()
 options.headless = True #Hide the Gui of the browser
 
-def main(classNum, subjectName, termNum):
-    driverChrome = webdriver.Chrome('/Users/keval/Downloads/chromedriver_mac64/chromedriver')
+def getClassesFromPage(classNum, subjectName, termNum):
+    driverChrome = webdriver.Chrome("/snap/bin/chromium.chromedriver")
     wait = WebDriverWait(driverChrome, 10) #Wait for up to 10 seconds
+
     driverChrome.get(f"https://catalog.apps.asu.edu/catalog/classes/classlist?campusOrOnlineSelection=A&catalogNbr={classNum}&honors=F&promod=F&searchType=all&subject={subjectName}&term={termNum}")
+    #Getting the site for the class we want
+    element = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[3]/div/div/div[5]/div/div/div")))
+    text = (element.text)
+    driverChrome.close()
+    return text
+
+def classNumberSearcher(classNumber, termNum):
+    url = (f"https://catalog.apps.asu.edu/catalog/classes/classlist?campusOrOnlineSelection=C&honors=F&keywords={classNumber}&promod=F&searchType=all&term={termNum}")
+    driverChrome = webdriver.Chrome("/snap/bin/chromium.chromedriver")
+    wait = WebDriverWait(driverChrome, 10) #Wait for up to 10 seconds
+    driverChrome.get(url)
+    #Getting the site for the class we want
     element = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[3]/div/div/div[5]/div/div/div")))
     text = (element.text)
     driverChrome.close()
     return text
 
 def csvWriter(text):
-    rows = [text.split('\n')[i:i+12] for i in range(0, len(text.split('\n')), 12)]
-    print(rows)
-    with open('output.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        for i, row in enumerate(rows):
-            if 'Add' in row:
-                writer.writerow(row[:row.index('Add')+1])
-                writer.writerow(row[row.index('Add')+1:])
-            else:
-                writer.writerow(row)
-
-def generate_availability_list(class_num, subject_name, term_num):
-    # Get the input text data and split it into lines
-    text = main(class_num, subject_name, term_num)
+    #Split the text into a list of strings
     lines = text.split('\n')
 
-    # Find the lines containing the desired subject and class number
-    class_header = subject_name + " " + str(class_num)
-    class_lines = [line for line in lines if line.strip(' ') == class_header]
+    #Create an empty list to store the extracted data
+    class_data = []
 
-    # Extract availability information from each class line
-    availability_list = []
-    for class_line in class_lines:
-        class_info = []
-        try:
-            # Look for availability information in the next 11 lines
-            for i in range(11):
-                line = lines[lines.index(class_line) + i]
-                if line.strip(' ') == 'ASU Online' or line.strip(' ') == 'Internet - Hybrid':
-                    # If availability info is found, add it to the class info list
-                    class_info.extend([line, lines[lines.index(line) + 1], lines[lines.index(line) + 2], lines[lines.index(line) + 3]])
-                    break
-                else:
-                    class_info.append(line)
-        except:
+    #Iterate through the lines and extract data for the specified subject and class number
+    for i in range(0, len(lines)):
+        if lines[i] == (subjectName + " " + str(classNum)):
+            data = []
+            try:
+                for j in range(0, 11):
+                    if (lines[i+j].strip(' ') == 'ASU Online') or (lines[i+j].strip(' ') == 'Internet - Hybrid'):
+                        data.append(lines[i+j])
+                        data.append(lines[i+j+1])
+                        data.append(lines[i+j+2])
+                        data.append(lines[i+j+3])
+                        break
+                    else:
+                        data.append(lines[i+j])
+            except:
+                continue
+            class_data.append(data)
+        else:
             continue
-        # If availability info was found, add it to the availability list
-        if class_info:
-            availability = class_info[-1]
-            availability_list.append([class_info[0], class_info[2], "Available" if int(availability.split()[0]) > 0 else "Not Available", availability])
 
-    # Print the availability list
-    for availability_info in availability_list:
-        print(availability_info)
+    columns = ['Class', 'Class Number', 'Availability','Seats']
+    availability_list = [] #Create a list to store availability data
+    for data in class_data: #Iterate through each extracted class and check if seats are available
+        class_info = [[]] # Initialize an empty nested list
+        availability = (data[-1])
+        s = availability.split(" ")
+        class_info[0].append(data[0]) #Append each element to the nested list
+        class_info[0].append(data[2])
+        if int(s[0]) > 0:
+            availability_status = "Available"
+        else:
+            availability_status = "Not Available"
+        class_info[0].append(availability_status)
+        class_info[0].append(data[-1])
+        availability_list.append(class_info[0])
+    with open('output.csv', 'w') as f:
+        for class_info in availability_list:
+            df = pd.DataFrame([class_info], columns=columns)
+            df.to_csv(f, header=f.tell()==0, index=False)
+    
 
-generate_availability_list(classNum, subjectName, termNum)
+
+# text = getClassesFromPage(classNum, subjectName, termNum)
+text = classNumberSearcher(classNumber ,termNum)
+csvWriter(text)
+
